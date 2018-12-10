a2034158399deffe0137eb04ff598d2f00723862
Andres Gomez <andresgomezjr89@gmail.com>
2018-12-10 05:19:46 -0500

    Added posting and commenting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  
+  resources :posts do
+
+    resources :comments
+  
+  end
+  
+  #use custom controllers ( our registration controller )
+  devise_for :users, controllers: { registrations: 'registrations' }
+  
+  root 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
